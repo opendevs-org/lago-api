f4966af2cb905030585e68f0b10e139543d80895
Romain Sempé <romain@getlago.com>
2024-07-10 14:16:26 +0200

    misc: Force external_subscription_id when creating events

--- app/serializers/v1/legacy/event_error_serializer.rb
+++ app/serializers/v1/legacy/event_error_serializer.rb
@@ -8,7 +8,6 @@ module V1
           input_params: {
             transaction_id: model.transaction_id,
             external_subscription_id: model.external_subscription_id,
-            external_customer_id: model.external_customer_id,
             timestamp: model.timestamp.to_f,
             code: model.code,
             properties: model.properties
