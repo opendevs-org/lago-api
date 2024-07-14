0d018a2281afc918f30bd4dfa33b8e1c03aae6a2
Mihir <mihirpandey.13@gmail.com>
2024-07-14 19:26:53 +0530

    chore: Comment out unused code in LagoUtils License module

--- lib/lago_utils/lago_utils/license.rb
+++ lib/lago_utils/lago_utils/license.rb
@@ -10,8 +10,8 @@ module LagoUtils
     def verify
       return if ENV['LAGO_LICENSE'].blank?
 
-      http_client = LagoHttpClient::Client.new("#{url}/verify/#{ENV["LAGO_LICENSE"]}")
-      response = http_client.get
+      # http_client = LagoHttpClient::Client.new("#{url}/verify/#{ENV["LAGO_LICENSE"]}")
+      # response = http_client.get
 
       @premium = true
     end
